e983995bf09f72413cf7dc29868d9155d546f0e9
vidorchan <1282911204@qq.com>
2020-11-20 01:16:41 +0800

    SpringMVC初始化
    页面使用jsp

---
+++ SpringMVC/src/main/java/com/vidor/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.vidor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
